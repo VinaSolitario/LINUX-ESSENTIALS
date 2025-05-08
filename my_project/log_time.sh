0041d218c3e64716534d0ef1e9a5f50c690725c1
Vina Solitario <152467094+VinaSolitario@users.noreply.github.com>
2025-05-08 15:57:54 +0800

    Delete my_project directory

--- my_project/log_time.sh
+++
@@ -1,9 +0,0 @@
-GITHUB_USER=$(echo $GITHUB_REPOSITORY | cut -d '/' -f1)
-
-echo "Current Date and Time: $(TZ=Asia/Manila date '+%a %b %d, %Y %H:%M:%S') - Logged by: $GITHUB_USER" >> log.txt
-
-COUNT=$(grep -c '^Current' log.txt) 
-
-tail -n +1 log.txt | grep -v "Total logs recorded" > temp_log.txt
-echo "Total logs recorded: $COUNT" >> temp_log.txt
-mv temp_log.txt log.txt
