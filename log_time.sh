3ba61104f4e191081621de88e9a9fbc70bc2fa3f
Vina Solitario <152467094+VinaSolitario@users.noreply.github.com>
2025-05-08 16:24:51 +0800

    Delete log_time.sh

--- log_time.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-GITHUB_USER=$(echo $GITHUB_REPOSITORY | cut -d '/' -f1)
-
-echo "Current Date and Time: $(TZ=Asia/Manila date '+%a %b %d, %Y %H:%M:%S') - Logged by: $GITHUB_USER" >> log.txt
