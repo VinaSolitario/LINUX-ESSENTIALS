5bcd1efbbf6f0f600cdcd8d92fd970cecd78be8f
Vina Solitario <152467094+VinaSolitario@users.noreply.github.com>
2025-05-08 07:37:22 +0000

    Add log_time.sh

--- my_project/log_time.sh
+++ my_project/log_time.sh
@@ -1,1 +1,9 @@
-echo "Current Date and Time: $(date)" >> log.txt
+GITHUB_USER=$(echo $GITHUB_REPOSITORY |  cut -d '/' -f1
+
+echo "Current Date and Time: $(TZ=Asia/Manila date '+%a %b %d, %Y %H:%M:%S') - Logged by: $GITHUB_USER" >> log.txt
+
+COUNT=$(grep -c '^Current' log.txt) 
+
+tail -n +1 log.txt | grep -v "Total logs recorded" > temp_log.txt
+echo "Total logs recorded: $COUNT" >> temp_log.txt
+mc temp_log.txt log.txt 
